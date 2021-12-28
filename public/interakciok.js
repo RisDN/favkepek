b058adeb91bb96c09f0f510aebff8d223bf2dfb2
Ris <r.andriska05@gmail.com>
2021-12-28 20:57:39 +0100

    interakciok modal

--- public/interakciok.js
+++ public/interakciok.js
@@ -8,4 +8,32 @@ function kepTorles(id) {
     }
     localStorage.setItem("favkepek", JSON.stringify(vissza))
     document.getElementById(id).remove()
+    $('#interakciok').modal('hide')
+}
+
+function kepTorlesMenu(id, nev) {
+    document.getElementById("szerkesztes-footer").className = "inaktiv"
+    document.getElementById("torles-footer").className = "aktiv"
+    document.getElementById("szerkesztes-cim").innerHTML = `„${nev}” törlése`
+    document.querySelector("#torles-gomb").onclick = function() { kepTorles(id) }
+    $('#interakciok').modal('show')
+    document.getElementById("szerkesztes-body").innerHTML = "<p>Biztosan kiszeretnéd törölni ezt a képet? <br>Ez a művelet nem visszafordítható!</p>"
+}
+
+function kepSzerkesztesMenu(id, nev, leiras, src) {
+    document.getElementById("szerkesztes-footer").className = "aktiv"
+    document.getElementById("torles-footer").className = "inaktiv"
+    document.getElementById("szerkesztes-cim").innerHTML = `„${nev}” szerkesztése`
+    document.querySelector("#szerkesztes-gomb").onclick = function() { kepSzerkesztes(id) }
+    $('#interakciok').modal('show')
+    document.getElementById("szerkesztes-body").innerHTML =
+        `
+        <input value="${nev}" type="text" name="" id="szerkesztes-menu-nev">
+        <input value="${leiras}" type="text" name="" id="szerkesztes-menu-leiras">
+        <input value="${src}" type="text" name="" id="szerkesztes-menu-src">
+        `
+}
+
+function kepSzerkesztes(id) {
+
 }
--- public/script.js
+++ public/script.js
@@ -1,30 +1,5 @@
 const LEIRAS_MAX_KARAKTER = 84
 
-const teszt = [{
-        id: 12312312,
-        nev: "Macska",
-        leiras: "Ez a kép egy igazan aranyos macskarol szol, sosem lattam meg ilyen aranyos macskat",
-        src: "https://www.pet4you.hu/img/big/1048.jpg"
-    },
-    {
-        id: 12312333,
-        nev: "Kutya",
-        leiras: "A kutyakat nem szeretjuk fuj kutya",
-        src: "https://www.purina.hu/proplan/sites/default/files/styles/nppe_article_listing_image_and_description/public/2020-11/GondosGazdik_blog_oregkutya5.jpg%20%201_2.jpg?itok=tHFTiHx7"
-    },
-    {
-        id: 331232123,
-        nev: "Papagáj geci",
-        leiras: "csipogogeci",
-        src: "https://www.zooplus.hu/magazin/wp-content/uploads/2018/11/A-Nimfapapag%C3%A1jok-768x768.jpg"
-    },
-    {
-        id: 321123332,
-        nev: "budosbogar",
-        leiras: "budos geci aki bemaszik es megijeszt ejszaka",
-        src: "https://ripost.hu/wp-content/uploads/kepadatbazis/thumbs/2017-10-11/3ddf240ae8cbfa5f4b428234ffafc2a7/620x0.jpg"
-    }
-]
 
 const adat = localStorage.getItem("favkepek")
 if (adat == null) {
@@ -62,8 +37,8 @@ function kepBetoltes(kepadat) {
         <img width="200" height="200" src="${kep_src}" alt="${kep_nev}">
 
         <div class="kep-interakciok">
-            <i onclick="kepSzerkesztes(${kep_id})" class="fa fa-pencil-square-o text-info" title="„${kep_nev}” szerkesztése" style="float: left;"></i>
-            <i onclick="kepTorles(${kep_id})" class="fa fa-trash text-danger" title="„${kep_nev}” törlése" style="float: right;"></i>
+            <i onclick="kepSzerkesztesMenu(${kep_id}, '${kep_nev}', '${kep_leiras}', '${kep_src}')" class="fa fa-pencil-square-o text-info" title="„${kep_nev}” szerkesztése" style="float: left;"></i>
+            <i onclick="kepTorlesMenu(${kep_id}, '${kep_nev}')" class="fa fa-trash text-danger" title="„${kep_nev}” törlése" style="float: right;"></i>
         </div>
 
         <div class="kep-leiras">
